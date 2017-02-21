e26fe5623a55d307b1cde159a5d72af512b3bd0a
Bruce Becker <brucellino@gmail.com>
2017-02-21 12:21:41 +0100

    Catch SIGUSR1

--- check-build.sh
+++ check-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 . /etc/profile.d/modules.sh
+
 module load ci
 module add gcc/${GCC_VERSION}
 cd ${WORKSPACE}/${NAME}-${VERSION}/
@@ -43,16 +44,23 @@ echo "Testing cblas"
 # get the file
 wget https://gist.githubusercontent.com/xianyi/6930656/raw/1b5868547a5277729d33dac62678a66ff65256f3/test_cblas_dgemm.c
 # Compile it
-echo "compiling with gcc. using"
+echo "compiling with gcc. using gcc -o cblas-test -I${OPENBLAS_DIR}/include -L${OPENBLAS_DIR}/lib test_cblas_dgemm.c -lopenblas -lpthread -lgfortran"
 gcc -o cblas-test -I${OPENBLAS_DIR}/include -L${OPENBLAS_DIR}/lib test_cblas_dgemm.c -lopenblas -lpthread -lgfortran
 echo "executing !"
 # EXECUTE !
-./cblas-test > cblas-test.out
-
+#  APPARENTLY this returns SIGUSR1 if executed properly, so we need to catch that.
+  (
+  set +e
+  ./cblas-test > test.out
+  if [ "$?" -eq "10"  ] ; then
+    echo "Everything is fine"
+    exit 0;
+  fi
+  )
 echo "Testing FORTRAN interface in c"
 #  Get the file
 wget https://gist.githubusercontent.com/xianyi/5780018/raw/c1d93058a2f61b88b9dd4237d2cf4a963065070b/time_dgemm.c
 # compile it
 gcc -o time_dgemm -fopenmp time_dgemm.c ${OPENBLAS_DIR}/lib/libopenblas.a
 # execute it
-time ./time_dgemm 1000 1000 1000s > time_dgemm.out
+time ./time_dgemm 1000 1000 1000s > time_dgemm.out > test.out
